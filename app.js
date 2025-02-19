de28b96b6b31c38ea7dd722c53db1280e75fd241
Paulo Estrela <dt.paulohenrique@gmail.com>
2025-02-18 22:12:30 -0300

    Cria uma função para adicionar nomes no Array

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let listaDeAmigos = [];
+let listaDeNomes = [];
+let amigo = document.getElementById("amigo").value;
+let amigosAdicionados = document.getElementById("listaAmigos");
+
+function adicionarAmigo() {
+    
+    let amigo = document.getElementById("amigo").value;
+    listaDeNomes.push(amigo);
+}
